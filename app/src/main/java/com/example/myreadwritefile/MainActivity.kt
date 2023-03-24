b3042babbafca94fe3ad79a214f01237d7e85498
Kevin Alfito <kevinalfito69@gmail.com>
2023-03-25 01:18:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myreadwritefile/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.myreadwritefile
+
+import android.icu.text.CaseMap.Title
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.myreadwritefile.databinding.ActivityMainBinding
+import com.example.myreadwritefile.model.FileHelper
+import com.example.myreadwritefile.model.FileModel
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonNew.setOnClickListener { newFile() }
+        binding.buttonOpen.setOnClickListener { showList() }
+        binding.buttonSave.setOnClickListener { saveFile() }
+    }
+    private fun newFile(){
+        binding.editTitle.setText("")
+        binding.editFile.setText("")
+        Toast.makeText(this, "Clearing file", Toast.LENGTH_SHORT).show()
+    }
+    private fun showList(){
+        val items = fileList()
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Pilih file yang diinginkan")
+        builder.setItems(items){dialog,item -> loadData(items[item].toString())}
+        val alert = builder.create()
+        alert.show()
+    }
+    private fun loadData(title: String){
+        val fileModel = FileHelper.readFromFile(this, title)
+        binding.editTitle.setText(fileModel.fileName)
+        binding.editFile.setText(fileModel.data)
+        Toast.makeText(this,"Loading ${fileModel.fileName} data", Toast.LENGTH_SHORT).show()
+    }
+    private fun saveFile(){
+        when{
+            binding.editTitle.text.toString().isEmpty() -> Toast.makeText(this, "Title harus diisi terlebih dahulu",Toast.LENGTH_SHORT).show()
+            binding.editFile.text.toString().isEmpty() -> Toast.makeText(this, "Konten harus diisi terlebih dahulu",Toast.LENGTH_SHORT).show()
+            else ->{
+                val title = binding.editTitle.text.toString()
+                val text = binding.editFile.text.toString()
+                val fileModel = FileModel()
+                fileModel.fileName = title
+                fileModel.data = text
+                FileHelper.writeToFile(fileModel, this)
+                Toast.makeText(this, "Saving ${fileModel.fileName} file", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myreadwritefile/model/FileModel.kt
@@ -0,0 +1,6 @@
+package com.example.myreadwritefile.model
+
+data class FileModel(
+    var fileName: String? = null,
+    var data: String? = null
+)
